676b9c30707478c28fdd606ca519de9bf514a286
Manju <manju_kogunde@Manjus-MacBook.local>
2017-10-08 16:55:05 -0700

    Updated the logic

--- learn-java-iq/src/com/java/iq/datastructure/BinarySearchTree.java
+++ learn-java-iq/src/com/java/iq/datastructure/BinarySearchTree.java
@@ -13,14 +13,6 @@ public class BinarySearchTree {
 
 	private Node root;
 
-	public Node getRoot() {
-		return root;
-	}
-
-	public void setRoot(Node root) {
-		this.root = root;
-	}
-
 	public void search(int value) {
 		search(root, value);
 	}
@@ -52,25 +44,20 @@ public class BinarySearchTree {
 	/*
 	 * Performance: O(log n)
 	 * 
-	 * 1. Start at the root node as current node.
-	 * 2. If the search key’s value matches the current node’s key then found a match
+	 * 1. Start at the root node as current node. 2. If the search key’s value matches the current node’s key then found a match
 	 * 
-	 * 3. If search key’s value is greater than current node’s
-	 * a. If the current node has a right child, search right
-	 * b. Else, no matching node in the tree
+	 * 3. If search key’s value is greater than current node’s a. If the current node has a right child, search right b. Else, no matching node in the tree
 	 * 
-	 * 4. If search key is less than the current node’s
-	 * a. If the current node has a left child, search
-	 * b. Else, no matching node in the tree
+	 * 4. If search key is less than the current node’s a. If the current node has a left child, search b. Else, no matching node in the tree
 	 */
 	private void search(Node n, int value) {
-		if (root != null) {
-			if (root.getValue() == value) {
-				System.out.println("Found value - " + root.getValue());
-			} else if (value < root.getValue()) {
-				search(root.getLeft(), value);
+		if (n != null) {
+			if (n.value == value) {
+				System.out.println("Found value - " + n.value);
+			} else if (value < n.value) {
+				search(n.left, value);
 			} else {
-				search(n.getRight(), value);
+				search(n.right, value);
 			}
 
 		}
@@ -83,30 +70,26 @@ public class BinarySearchTree {
 	 * 
 	 * 2. Start at root node as current node
 	 * 
-	 * 3. If new node’s key < current’s key
-	 * a. If current node has a left child, search left
-	 * b. Else add new node as current’s left child
+	 * 3. If new node’s key < current’s key a. If current node has a left child, search left b. Else add new node as current’s left child
 	 * 
-	 * 4. If new node’s key > current’s key
-	 * a. If current node has a right child, search right
-	 * b. Else add new node as current’s right child
+	 * 4. If new node’s key > current’s key a. If current node has a right child, search right b. Else add new node as current’s right child
 	 */
 	private void insert(Node n, int value) {
 		if (n != null) {
-			if (value < n.getValue()) {
-				if (n.getLeft() != null) {
-					insert(n.getLeft(), value);
+			if (value < n.value) {
+				if (n.left != null) {
+					insert(n.left, value);
 				} else {
 					Node newNode = new Node(value);
-					n.setLeft(newNode);
+					n.left = newNode;
 				}
 			}
-			if (value > n.getValue()) {
-				if (n.getRight() != null) {
-					insert(n.getRight(), value);
+			if (value > n.value) {
+				if (n.right != null) {
+					insert(n.right, value);
 				} else {
 					Node newNode = new Node(value);
-					n.setRight(newNode);
+					n.right = newNode;
 				}
 			}
 
@@ -119,119 +102,84 @@ public class BinarySearchTree {
 	 * Performance: O(log n).
 	 */
 	private int minValue(Node n) {
-		while (n.getLeft() != null) {
-			n = n.getLeft();
+		while (n.left != null) {
+			n = n.left;
 		}
 
-		return n.getValue();
+		return n.value;
 	}
 
 	/*
 	 * Performance: O(log n).
 	 */
 	private int maxValue(Node n) {
-		while (n.getRight() != null) {
-			n = n.getRight();
+		while (n.right != null) {
+			n = n.right;
 		}
-		return n.getValue();
+		return n.value;
 	}
 
 	/*
-	 * In-order:
-	 * Below process is applied recursively to all the node in the tree,
-	 * until either the left sub tree is empty or the right sub tree is empty.
+	 * Pre-order: Below process is applied recursively to all the node in the tree, until either the left sub tree is empty or the right sub tree is empty.
 	 * 
-	 * 1. Left sub tree of the given node is visited first
-	 * 2. Value at the given node is printed
-	 * 3. Right sub tree of the given node is visited
-	 * 
-	 * Eg: 1,2,3,4,5
-	 */
-	private void inorderTraversal(Node n) {
-		if (n == null) {
-			return;
-		}
-		inorderTraversal(n.getLeft());
-		System.out.println(n.getValue() + " ");
-		inorderTraversal(n.getRight());
-	}
-
-	/*
-	 * Pre-order:
-	 * Below process is applied recursively to all the node in the tree,
-	 * until either the left sub tree is empty or the right sub tree is empty.
-	 * 
-	 * 1. Value at the given node is printed first
-	 * 2. Left sub tree of the given node is visited
-	 * 3. Right sub tree of the given node is visited
-	 * 
-	 * Eg: 1,2,3,4,5
+	 * 1. Value at the given node is printed first 2. Left sub tree of the given node is visited 3. Right sub tree of the given node is visited
+	 *
+	 * 1 2 3
 	 */
 	private void preorderTraversal(Node n) {
 		if (n == null) {
 			return;
 		}
+		System.out.println(n.value + " ");
+		preorderTraversal(n.left);
+		preorderTraversal(n.right);
+	}
 
-		System.out.println(n.getValue() + " ");
-		preorderTraversal(n.getLeft());
-		preorderTraversal(n.getRight());
+	/*
+	 * In-order: Below process is applied recursively to all the node in the tree, until either the left sub tree is empty or the right sub tree is empty.
+	 * 
+	 * 1. Left sub tree of the given node is visited first 2. Value at the given node is printed 3. Right sub tree of the given node is visited
+	 * 
+	 * 2 1 3
+	 */
+	private void inorderTraversal(Node n) {
+		if (n == null) {
+			return;
+		}
+		inorderTraversal(n.left);
+		System.out.println(n.value + " ");
+		inorderTraversal(n.right);
 	}
 
 	/*
 	 * Post-order:
 	 * 
-	 * Below process is applied recursively to all the node in the tree,
-	 * until either the left sub tree is empty or the right sub tree is empty.
+	 * Below process is applied recursively to all the node in the tree, until either the left sub tree is empty or the right sub tree is empty.
 	 * 
-	 * 1. Left sub tree of the given node is visited first
-	 * 2. Right sub tree of the given node is visited
-	 * 3. Value at the given node is printed
+	 * 1. Left sub tree of the given node is visited first 2. Right sub tree of the given node is visited 3. Value at the given node is printed
+	 * 
+	 * 3 1 2
 	 */
 	private void postorderTraversal(Node n) {
 		if (n == null) {
 			return;
 		}
-
-		postorderTraversal(n.getLeft());
-		postorderTraversal(n.getRight());
-		System.out.println(n.getValue() + " ");
+		postorderTraversal(n.left);
+		postorderTraversal(n.right);
+		System.out.println(n.value + " ");
 	}
 
-}
+	class Node {
 
-class Node {
+		private Node left;
+		private Node right;
+		private int value;
 
-	private Node left;
-	private Node right;
-	private int value;
+		public Node(int value) {
+			super();
+			this.value = value;
+		}
 
-	public Node(int value) {
-		super();
-		this.value = value;
-	}
-
-	public Node getLeft() {
-		return left;
-	}
-
-	public Node getRight() {
-		return right;
-	}
-
-	public int getValue() {
-		return value;
-	}
-
-	public void setLeft(Node left) {
-		this.left = left;
-	}
-
-	public void setRight(Node right) {
-		this.right = right;
-	}
-
-	public void setValue(int value) {
-		this.value = value;
 	}
 
 }
--- learn-java-iq/src/com/java/iq/datastructure/MyLinkedList.java
+++ learn-java-iq/src/com/java/iq/datastructure/MyLinkedList.java
@@ -13,101 +13,99 @@ public class MyLinkedList {
 
 	public boolean remove(String item) {
 		if (item != null) {
-			for (MyNode x = first; x != null; x = x.getNext()) {
-				unlink(item, x);
-				return true;
-			}
+			MyNode temp = first;
+			unlink(item, temp);
+			// for (MyNode x = first; x != null; x = x.next) {
+			// unlink(item, x);
+			// return true;
+			// }
 		}
 		return false;
 	}
 
-	private void unlink(String item, MyNode x) {
-		if (x.getItem().equals(item)) {
-			MyNode prev = x.getPrev();
-			MyNode next = x.getNext();
-
-			if (prev == null) {
-				first.setNext(next);
-			} else {
-				prev.setNext(next);
-				x.setPrev(null);
+	public void display() {
+		MyNode temp = first;
+		final StringBuilder builder = new StringBuilder("[");
+		while (temp != null) {
+			builder.append(temp.item);
+			temp = temp.next;
+			if (temp != null) {
+				builder.append(", ");
 			}
-
-			if (next == null) {
-				last = prev;
-			} else {
-				next.setPrev(next);
-				x.setNext(null);
-			}
-			x.setItem(null);
-			size--;
 		}
+		builder.append("]");
+		System.out.println(builder.toString());
 	}
 
-	private void linkLast(String item) {
-		MyNode lastTemp = last;
-		MyNode newNode = new MyNode(last, item, null);
-		last = newNode;
-		if (lastTemp == null) {
-			first = newNode;
+	private void unlink(String item, MyNode currNode) {
+		while (currNode != null) {
+			MyNode prevNode = currNode.prev;
+			MyNode nextNode = currNode.next;
+
+			if (currNode.item.equals(item)) {
+				if (prevNode == null) {
+					first = nextNode;
+				} else {
+					prevNode.next = nextNode;
+				}
+				if (nextNode == null) {
+					last = prevNode;
+				} else {
+					nextNode.prev = prevNode;
+				}
+
+				currNode = null;
+				size--;
+				return;
+			}
+			currNode = currNode.next;
+		}
+
+	}
+
+	private void linkLast(final String item) {
+		final MyNode n = new MyNode(last, item, null);
+		if (first == null) {
+			first = n;
+			last = n;
 		} else {
-			last.setNext(newNode);
+			last.next = n;
+			last = n;
 		}
 		size++;
 	}
 
-	public MyNode getFirst() {
-		return first;
+	class MyNode {
+
+		private MyNode prev;
+		private String item;
+		private MyNode next;
+
+		public MyNode() {
+
+		}
+
+		public MyNode(MyNode prev, String item, MyNode next) {
+			this.prev = prev;
+			this.item = item;
+			this.next = next;
+		}
 	}
 
-	public MyNode getLast() {
-		return last;
-	}
+	public static void main(String[] args) {
+		final MyLinkedList linkedList = new MyLinkedList();
+		linkedList.add("a");
+		linkedList.add("b");
+		linkedList.add("c");
+		linkedList.add("d");
+		System.out.println("My Linked List: ");
+		linkedList.display();
 
-	public int getSize() {
-		return size;
-	}
+		System.out.println("Removing item: a");
+		linkedList.remove("a");
 
-}
-
-class MyNode {
-
-	private MyNode prev;
-	private String item;
-	private MyNode next;
-
-	public MyNode() {
-
-	}
-
-	public MyNode(MyNode prev, String item, MyNode next) {
-		this.prev = prev;
-		this.item = item;
-		this.next = next;
-	}
-
-	public MyNode getPrev() {
-		return prev;
-	}
-
-	public String getItem() {
-		return item;
-	}
-
-	public MyNode getNext() {
-		return next;
-	}
-
-	public void setPrev(MyNode prev) {
-		this.prev = prev;
-	}
-
-	public void setItem(String item) {
-		this.item = item;
-	}
-
-	public void setNext(MyNode next) {
-		this.next = next;
+		System.out.println("My Linked List: ");
+		linkedList.display();
 	}
 
 }
