476bcc2b1925d39b107f2d3fd59b97cc0dd8127c
Manju <manju_kogunde@Manjus-MacBook.local>
2017-10-23 13:27:15 -0700

    Updated the text

--- learn-java-iq/src/com/java/iq/test/TestJava.java
+++ learn-java-iq/src/com/java/iq/test/TestJava.java
@@ -1,38 +1,48 @@
 package com.java.iq.test;
 
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Scanner;
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
 
 public class TestJava {
 
+	private final Character m_value = 'a';
+
+	public String toString() {
+
+		return "" + m_value;
+	}
+
 	public static void main(String[] args) {
-
-		List<Integer> test = new LinkedList<Integer>();
-		// test.rem
-
-		// 121
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("Enter the text : ");
-		String line = scanner.nextLine();
-
-		char[] charArray = line.toCharArray();
-
-		StringBuilder sb = new StringBuilder();// Hello --> Helo
-
-		for (int i = 0; i < charArray.length; i++) {
-			for (int j = i + 1; j < charArray.length; j++) {
-				if (charArray[i] == charArray[j]) {
-					charArray[j] = 0;
-				}
+		
+		
+		Map<Integer, String> map = new HashMap<Integer, String>(5);
+		map.put(1, "apple");
+		map.put(2, null);
+		map.put(new Integer(3), "peach");
+		map.put(3, "orange");
+		map.put(4, "peach");
+		
+		
+		for(String s : map.values()){
+			if("orange".equals(s)){
+				map.put(5, "banana");
 			}
-			if (charArray[i] != 0) {
-				sb.append(charArray[i]);
-			}
+			
 		}
+		System.out.println(map.get(5));
+		
+		
 
-		System.out.println(sb.toString());
-
-		scanner.close();
+//		try {
+//			Field f = TestJava.class.getDeclaredField("m_value");
+//			f.setAccessible(true);
+//			TestJava a = new TestJava();
+//			f.set(a, (char) 'b');
+//			System.out.println(a);
+//
+//		} catch (Exception e) {
+//
+//		}
 	}
 }
